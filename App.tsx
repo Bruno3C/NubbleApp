ba7356c727d34552bb8198ee9a71b1bfeae3a1a0
Bruno3C <brunodossantos.job@gmail.com>
2023-09-03 10:38:57 -0300

    :sparkles: feat: primeiro componente

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+import { Text } from './src/components/Text/Text';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+     <Text>Nubble App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
