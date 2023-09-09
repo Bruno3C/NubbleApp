bca019294d9870112c827d186e0dfe4ea703fb5b
Bruno3C <brunodossantos.job@gmail.com>
2023-09-09 10:30:34 -0300

    :sparkles: feat: restyle - criando tema do app

--- App.tsx
+++ App.tsx
@@ -3,12 +3,17 @@ import {
   SafeAreaView,
 } from 'react-native';
 import { Text } from './src/components/Text/Text';
+import { ThemeProvider, useTheme } from '@shopify/restyle';
+import { Theme } from './src/theme/theme';
 
 function App(): JSX.Element {
+  const theme = useTheme<Theme>();
   return (
-    <SafeAreaView>
-     <Text>Nubble App</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme} >
+      <SafeAreaView>
+      <Text>Nubble App</Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
