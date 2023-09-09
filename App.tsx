5d813cf0f727edc007624743f8ff28f14b195c26
Bruno3C <brunodossantos.job@gmail.com>
2023-09-09 11:10:05 -0300

    :sparkles: feat: TouchableOpacityBox - restyle custom component

--- App.tsx
+++ App.tsx
@@ -3,15 +3,24 @@ import {
   SafeAreaView,
 } from 'react-native';
 import { Text } from './src/components/Text/Text';
-import { ThemeProvider, useTheme } from '@shopify/restyle';
-import { Theme } from './src/theme/theme';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from './src/theme/theme';
+import { TouchableOpacityBox } from './src/components/Box/Box';
 
 function App(): JSX.Element {
-  const theme = useTheme<Theme>();
   return (
     <ThemeProvider theme={theme} >
-      <SafeAreaView>
-      <Text>Nubble App</Text>
+      <SafeAreaView style={{ padding: 10}}>
+      <TouchableOpacityBox
+        backgroundColor='buttonPrimary'
+        borderRadius='s16'
+        justifyContent='center'
+        alignItems='center'
+        activeOpacity={.7}
+        p='s20'
+      >
+        <Text preset='headingSmall' style={{ color: 'white'}} >Button</Text>
+      </TouchableOpacityBox>
       </SafeAreaView>
     </ThemeProvider>
   );
