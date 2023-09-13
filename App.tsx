885b79d728d25190afabcaa0c46541e23b7c0f87
Bruno3C <brunodossantos.job@gmail.com>
2023-09-13 19:46:15 -0300

    :sparkles: feat: primeiro icon svg

--- App.tsx
+++ App.tsx
@@ -3,12 +3,14 @@ import { SafeAreaView } from 'react-native';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 import { Button } from './src/components/Button/Button';
+import { EyeOffIcon } from './src/assets/icons/EyeOffIcon';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme} >
       <SafeAreaView style={{ padding: 10}}>
-        <Button title="Confirmar" loading/>
+        <Button title="Confirmar"/>
+        <EyeOffIcon/>
       </SafeAreaView>
     </ThemeProvider>
   );
