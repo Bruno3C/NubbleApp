0df453a35ea545e5fd60b666c110a9ba2cfe3b35
Bruno3C <brunodossantos.job@gmail.com>
2023-09-13 20:21:09 -0300

    :sparkles: feat: icon component

--- App.tsx
+++ App.tsx
@@ -4,13 +4,14 @@ import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
 import { Button } from './src/components/Button/Button';
 import { EyeOffIcon } from './src/assets/icons/EyeOffIcon';
+import { Icon } from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme} >
       <SafeAreaView style={{ padding: 10}}>
         <Button title="Confirmar"/>
-        <EyeOffIcon/>
+        <Icon name='eyeOn' />
       </SafeAreaView>
     </ThemeProvider>
   );
