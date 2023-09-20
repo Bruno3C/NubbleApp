b317baec0b96d683afb231d92900bc6e55d044f4
Bruno <brunodanielsantos.ti@gmail.com>
2023-09-20 10:07:01 -0300

    :sparkles: integrando no design system e definindo interface

--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -3,15 +3,15 @@ import {
   ActivityIndicator as RNActivityIndicator,
   ActivityIndicatorProps
 } from 'react-native';
-import { Theme, ThemeColors } from '../../theme/theme';
-import { useTheme } from '@shopify/restyle';
+import { ThemeColors } from '../../theme/theme';
+import { useAppTheme } from '../../hooks/useAppTheme';
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
   color: ThemeColors;
 }
 
 export function ActivityIndicator({ color}: Props) {
-  const {colors} = useTheme<Theme>();
+  const {colors} = useAppTheme();
   return <RNActivityIndicator color={colors[color]}/>
 }
 
