2b2ec8eceeac5e2ddf5d653bdf62c64df6260386
Bruno <brunodanielsantos.ti@gmail.com>
2023-10-04 16:09:48 -0300

    :sparkles: customização bottom tab - estilização e refinamento

--- src/routes/AppTabBar.tsx
+++ src/routes/AppTabBar.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
 
-import {Box, Icon, Text, TouchableOpacityBox} from '@components';
+import {
+  Box,
+  BoxProps,
+  Icon,
+  Text,
+  TextProps,
+  TouchableOpacityBox,
+  TouchableOpacityBoxProps,
+} from '@components';
+import {useAppSafeArea} from '@hooks';
 import {AppTabBottomTabParamList} from '@routes';
+import {$shadowProps} from '@theme';
 
 import {mapScreenToProps} from './mapScreenTopProps';
 
 export function AppTabBar({state, descriptors, navigation}: BottomTabBarProps) {
+  const {bottom} = useAppSafeArea();
   return (
-    <Box flexDirection="row">
+    <Box
+      {...$boxWrapper}
+      style={[{paddingBottom: bottom}, $shadowProps]}
+      flexDirection="row">
       {state.routes.map((route, index) => {
         const {options} = descriptors[route.key];
 
@@ -27,7 +41,11 @@ export function AppTabBar({state, descriptors, navigation}: BottomTabBarProps) {
 
           if (!isFocused && !event.defaultPrevented) {
             // The `merge: true` option makes sure that the params inside the tab screen are preserved
-            navigation.navigate({name: route.name, merge: true});
+            navigation.navigate({
+              name: route.name,
+              params: route.params,
+              merge: true,
+            });
           }
         };
 
@@ -40,9 +58,7 @@ export function AppTabBar({state, descriptors, navigation}: BottomTabBarProps) {
 
         return (
           <TouchableOpacityBox
-            activeOpacity={1}
-            alignItems="center"
-            accessibilityRole="button"
+            {...$itemWrapper}
             accessibilityState={isFocused ? {selected: true} : {}}
             accessibilityLabel={options.tabBarAccessibilityLabel}
             testID={options.tabBarTestID}
@@ -54,8 +70,7 @@ export function AppTabBar({state, descriptors, navigation}: BottomTabBarProps) {
               name={isFocused ? tabItem.icon.focused : tabItem.icon.unfocused}
             />
             <Text
-              semiBold
-              preset="paragraphCaption"
+              {...$label}
               color={isFocused ? 'primary' : 'backgroundContrast'}>
               {tabItem.label}
             </Text>
@@ -65,3 +80,21 @@ export function AppTabBar({state, descriptors, navigation}: BottomTabBarProps) {
     </Box>
   );
 }
+
+const $label: TextProps = {
+  semiBold: true,
+  marginTop: 's4',
+  preset: 'paragraphCaption',
+};
+
+const $itemWrapper: TouchableOpacityBoxProps = {
+  activeOpacity: 1,
+  alignItems: 'center',
+  accessibilityRole: 'button',
+};
+
+const $boxWrapper: BoxProps = {
+  paddingTop: 's12',
+  backgroundColor: 'background',
+  flexDirection: 'row',
+};
