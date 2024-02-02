140c55c5723d684fdb3331be6ee766468f3d6c15
Bruno3C <brunodossantos.job@gmail.com>
2024-02-02 16:19:01 -0300

    :sparkles: feat: integrando service na HomeScreen

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import {Post, postService} from '@domain';
 
 import {Button, Screen, Text} from '@components';
 import {AppTabScreenProps} from '@routes';
 
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
+  const [postList, setPostList] = useState<Post[]>([]);
+
+  useEffect(() => {
+    postService.getList().then(list => setPostList(list));
+  }, []);
+
   return (
     <Screen>
       <Text preset="headingLarge">Home Screen</Text>
