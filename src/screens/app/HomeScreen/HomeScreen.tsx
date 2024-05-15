ed0d1a57b7a491bf3a88a14b09426634c719fb18
Bruno3C <brunodossantos.job@gmail.com>
2024-05-15 09:30:37 -0300

    :sparkles: feat: estados de loading, erro e lista vazia

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -6,13 +6,29 @@ import {Post, postService} from '@domain';
 import {PostItem, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
 
+import {HomeEmpty} from './components/HomeEmpty';
 import {HomeHeader} from './components/HomeHeader';
 
 export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
   const [postList, setPostList] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<boolean | null>(null);
+
+  async function fetchData() {
+    try {
+      setError(null);
+      setLoading(true);
+      const list = await postService.getList();
+      setPostList(list);
+    } catch (er) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  }
   useEffect(() => {
-    postService.getList().then(list => setPostList(list));
-  }, [postList]);
+    fetchData();
+  }, []);
 
   function renderItem({item}: ListRenderItemInfo<Post>) {
     return <PostItem post={item} />;
@@ -22,15 +38,20 @@ export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
       <FlatList
         showsVerticalScrollIndicator={false}
         data={postList}
+        contentContainerStyle={{flex: postList.length === 0 ? 1 : undefined}}
         keyExtractor={item => item.id}
         renderItem={renderItem}
         ListHeaderComponent={<HomeHeader />}
+        ListEmptyComponent={
+          <HomeEmpty refetch={fetchData} error={error} loading={loading} />
+        }
       />
     </Screen>
   );
 }
 
 const $screen: StyleProp<ViewStyle> = {
+  flex: 1,
   paddingTop: 0,
   paddingBottom: 0,
   paddingHorizontal: 0,
